dc11e017498aebc966401960acb290d42c4de16a
haiming <haiming_liang@hotmail.com>
2020-10-01 10:22:02 +0800

    first commit

---
+++ src/main/java/com/haiming/springbuckscoffeeshop/SpringBucksCoffeeShopApplication.java
@@ -0,0 +1,13 @@
+package com.haiming.springbuckscoffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBucksCoffeeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBucksCoffeeShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haiming/springbuckscoffeeshop/SpringBucksCoffeeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haiming.springbuckscoffeeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBucksCoffeeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
